4f1dcbc33b84eb4d865eec394ed9fa68ed3f3e99
Ludvig Noring <ludvig.noring@gmail.com>
2024-11-19 17:03:28 +0100

    generation code used for mustache-gen PR

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@ import (
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
 
-//go:generate terraform fmt -recursive ../examples/
+// //go:generate terraform fmt -recursive ../examples/
 
 // Generate documentation.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name osc
